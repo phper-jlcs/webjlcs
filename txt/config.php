153d3eb9a260bc3dc85ad65fda8f6635cf32cecb
古鸿尹 <1322214496@qq.com>
2018-11-19 14:18:39 +0800

    1

---
+++ txt/config.php
@@ -0,0 +1,27 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'alipay_config'=>array(
+        'partner' =>'2088821708574743',   //这里是你在成功申请支付宝接口后获取到的PID；
+        'key'=>'5bajl18rkrofx8axjckxj3pszl8w2ar4',//这里是你在成功申请支付宝接口后获取到的Key
+        'sign_type'=>strtoupper('MD5'),
+        'input_charset'=> strtolower('utf-8'),
+        'cacert'=> getcwd().'\\cacert.pem',
+        'transport'=> 'http',
+    ),
+    //以上配置项，是从接口包中alipay.config.php 文件中复制过来，进行配置；
+
+    'alipay'   =>array(
+        //这里是卖家的支付宝账号，也就是你申请接口时注册的支付宝账号
+        'seller_email'=>'sanguogg@dingtalk.com',
+        //这里是异步通知页面url，提交到项目的Pay控制器的notifyurl方法；
+        'notify_url'=>'http://www.zgdigong.com/index.php/Home/Pay/notifyurl',
+        //这里是页面跳转通知url，提交到项目的Pay控制器的returnurl方法；
+        'return_url'=>'http://www.zgdigong.com/index.php/Home/Pay/returnurl',
+        //支付成功跳转到的页面，我这里跳转到项目的User控制器，myorder方法，并传参payed（已支付列表）
+        'successpage'=>'Pay/gongde',
+        //支付失败跳转到的页面，我这里跳转到项目的User控制器，myorder方法，并传参unpay（未支付列表）
+        'errorpage'=>'User/myorder?ordtype=unpay',
+    ),
+    'LOAD_EXT_FILE'=>'ifunction'
+);
